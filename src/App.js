391760586dacf94b0e923b4e5e89eebfc2653dc2
JuniorAlvess <junioralves.jav@gmail.com>
2021-10-20 14:44:49 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,186 @@
-import logo from './logo.svg';
 import './App.css';
-
+const THREE = require('three');
 function App() {
+  // Generic definitions - variables
+
+  var WIDTH = document.body.clientWidth, HEIGHT = document.body.clientHeight;
+
+  // var lockOrientation = window.screen.orientation.lock || window.screen.lockOrientation
+  //   || window.screen.mozLockOrientation || window.screen.msLockOrientation;
+
+  navigator.getMedia = (navigator.getUserMedia ||
+    navigator.webkitGetUserMedia ||
+    navigator.mozGetUserMedia ||
+    navigator.msGetUserMedia);
+
+  window.addEventListener("compassneedscalibration", function (event) {
+    window.self.supported = true;
+    alert('Your compass needs calibrating!');
+    event.preventDefault();
+  }, true);
+
+  if (!navigator.getMedia) {
+    alert("Oh noes! Your browser does not support webcam video :(");
+  }
+
+  var scene = new THREE.Scene(),
+    camera = new THREE.PerspectiveCamera(75, WIDTH / HEIGHT, 0.1, 1000),
+    renderer = new THREE.WebGLRenderer({ alpha: true }),
+    light, geometry, north, south, east, west, ctx,
+    canvas = document.getElementById("camera"),
+    video = document.querySelector("video"),
+    btn = document.getElementById("btn");
+
+  // Generic definitions functions
+
+  function deg2rad(angle) {
+    return (angle / 180.0) * Math.PI;
+  }
+
+  function draw() {
+    var width = document.body.clientWidth
+    var height = document.body.clientHeight;
+    var video = document.querySelector("video")
+    ctx.fillStyle = "#ff00ff";
+    ctx.fillRect(0, 0, width, height);
+    ctx.drawImage(video, 0, 0, width, height);
+
+    north.rotation.y += 0.05;
+    south.rotation.y += 0.05;
+
+    east.rotation.y += 0.05;
+    west.rotation.y += 0.05;
+
+    renderer.render(scene, camera);
+    requestAnimationFrame(draw);
+  }
+
+  function showWebcamVideo(sourceId) {
+    navigator.getMedia({
+      video: {
+        optional: [{ sourceId: sourceId }]
+      }
+    }, function onSuccess(stream) {
+      var video = document.querySelector('video');
+      video.src = window.URL.createObjectURL(stream);
+    }, function onError(err) {
+      alert("Whoops: " + err);
+      console.error(err);
+    });
+  }
+
+  function updateOrientation(e) {
+    var compassdir;
+    if (e.webkitCompassHeading) 
+    compassdir = e.webkitCompassHeading;
+    else compassdir = e.alpha;
+
+    var heading = compassdir,
+      pitch = e.gamma;
+
+    // Correcting the sensors being "clever"
+    if (Math.abs(e.beta) > 45) {
+      heading += 90;
+    } else {
+      heading -= 90;
+    }
+
+    if (pitch < 0) {
+      pitch = -90 - pitch;
+    } else {
+      pitch = 90 - pitch;
+    }
+
+    if (heading < 0) heading = 360 + heading;
+
+    camera.rotation.set(deg2rad(pitch), deg2rad(heading), 0);
+    e.preventDefault();
+  }
+
+  // Initialisiation and run!
+
+  camera.eulerOrder = 'YXZ'; // We want to rotate around the Y axis first or our perspective is screwed up
+
+  light = new THREE.PointLight(0xffffff, 1, 100);
+  scene.add(light);
+
+  geometry = new THREE.BoxGeometry(10, 10, 10);
+  north = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({ color: 0xff0000 }));
+  south = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({ color: 0x00ff00 }));
+  east = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({ color: 0x0000ff }));
+  west = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({ color: 0xffff00 }));
+
+  north.position.set(0, 0, -50);
+  south.position.set(0, 0, 50);
+
+  east.position.set(50, 0, 0);
+  west.position.set(-50, 0, 0);
+
+  scene.add(north);
+  scene.add(east);
+  scene.add(south);
+  scene.add(west);
+
+  window.addEventListener("deviceorientation", updateOrientation);
+
+  const handleButton = () => {
+    var canvas = document.getElementById("camera")
+    canvas.classList.remove('hidden')
+
+    if (document.body.webkitRequestFullscreen) document.body.webkitRequestFullscreen();
+    else if (document.body.mozRequestFullScreen) document.body.mozRequestFullScreen();
+
+    // document.body.removeChild(document.getElementById("instructions"));
+
+    document.body.appendChild(renderer.domElement);
+    renderer.setSize(WIDTH, HEIGHT);
+
+    // if (window.screen.orientation.lock && !window.screen.orientation.lock('landscape-primary')) {
+    //   alert("Please put your phone in landscape mode for this demo");
+    // } else if (window.screen.lockOrientation && !window.screen.lockOrientation('landscape-primary')) {
+    //   alert("Please put your phone in landscape mode for this demo");
+    // }
+
+    canvas.width = WIDTH;
+    canvas.height = HEIGHT;
+
+    ctx = canvas.getContext("2d");
+
+    // MediaStreamTrack.getSources(function (mediaSources) {
+    //   mediaSources.forEach(function (mediaSource) {
+    //     if (mediaSource.kind === 'video' && mediaSource.facing == "environment") {
+    //       showWebcamVideo(mediaSource.id);
+    //     }
+    //   });
+    // });
+
+    navigator.mediaDevices
+      .getUserMedia({ audio: false, video: { facingMode: 'environment' } })
+      .then((mediaStream) => {
+        const video = document.querySelector("video");
+
+        video.srcObject = mediaStream;
+        video.play();
+      })
+      .catch(function (err) {
+        alert("Not Camera allowed", err.message);
+      });
+
+
+    draw();
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div id="instructions">
+        <h1>Instructions</h1>
+        <p>Please hold your phone in landscape mode with the top of the phone in your left hand.</p>
+        <p>Allow fullscreen and access to the webcam for the demo to work properly.</p>
+        <button onClick={() => handleButton()}>Click me to start!</button>
+      </div>
+      <canvas className="hidden" id="camera" width="300" height="300"></canvas>
+      <video preload="metadata" muted playsInline></video>
+    </>
   );
 }
 
